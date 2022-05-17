40ee9f57b8c7a52a588217bb5d420085614fb6c4
clara-reyna <61567046+clara-reyna@users.noreply.github.com>
2022-05-17 19:41:54 -0300

    ciclo white

--- paginaInicio.js
+++ paginaInicio.js
@@ -1,12 +1,10 @@
-let cantTareas = prompt("How many issues do you want to view today?")
- console.log(cantTareas)
- if (cantTareas == false){
-     console.log("you have to write something")
- }
- let x = true 
- while(x){
-     cantTareas = prompt("How many issues do you want to view today?")
-     if (!(cantTareas == false || cantTareas == null)){
-         x =false
-     } 
-    }  
+let tareas = 10; 
+
+while (tareas > 0) {
+   
+    tareas--;
+
+    console.log ("tareas restante " + tareas)
+}
+
+console.log ("Ya no tienes tareas pendientes")
