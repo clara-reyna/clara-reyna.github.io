89a100784b552594ffde4e827df8d43f59703b56
clara-reyna <61567046+clara-reyna@users.noreply.github.com>
2022-05-17 19:47:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let nota = prompt("Ingrese un número")
-
-if (nota = 3){
-    console.log("prueba");
-}
+function login(){ 
+    var username; 
+    var password; 
+    var txtUsername; 
+    var txtPassword; 
+    txtUsername = document.getElementsByName("txtUsername")[0]; 
+    txtPassword = document.getElementsByName("txtPassword")[0]; 
+    username = txtUsername.value; 
+    password = txtPassword.value;  
+   
+    if (username=="clara.reyna" && password=="1234"){  
+    window.location.replace("https://clara-reyna.github.io/paginaInicio.html");
+    } else { 
+    alert("El valor ingresado en usuario y/o contraseña es incorrecto"); 
+    }}
